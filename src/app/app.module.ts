8e7a05d0aec8e0bb2d4646022a55d82c3e0f35ca
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-10 22:52:42 +0200

    Added Child (nested) Routes for Users and Servers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,17 @@ import { ServerComponent } from './servers/server/server.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'users/:id/:name', component: UserComponent},
-  {path: 'servers', component: ServersComponent},
-  {path: 'servers/:id', component: ServerComponent},
-  {path: 'servers/:id/edit', component: EditServerComponent}
+  {
+    path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent}
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent}
+    ]
+  }
 ];
 
 @NgModule({
