801e225c2f9de3f0dc0089ae465221557e693ec2
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-11 10:56:08 +0200

    Preserving the Query Parameters in the URI

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -28,7 +28,7 @@ export class ServerComponent implements OnInit {
   }
 
   onEdit(): void {
-    this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
 }
